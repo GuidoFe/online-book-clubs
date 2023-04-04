f1f793bdde06081ce6d8790d2bfaba7735a3b1c4
guidofe <guido.ferri@protonmail.com>
2023-04-04 22:32:24 +0200

    Initialized the project

---
+++ src/main/java/com/guidofe/onlinebookclubs/OnlinebookclubsApplication.java
@@ -0,0 +1,13 @@
+package com.guidofe.onlinebookclubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookclubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebookclubsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guidofe/onlinebookclubs/OnlinebookclubsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guidofe.onlinebookclubs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebookclubsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
